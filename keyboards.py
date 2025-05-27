b559c66c662547c86f404cc78aa3ed75b3486241
Hitslater <86098865+Hitslater@users.noreply.github.com>
2025-05-27 20:39:04 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,6 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
+from .keyboards import keyboard
 
 web_app = WebAppInfo(url="https://hitslater.github.io/")
 
@@ -11,5 +12,12 @@ keyboard = types.ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
+@dp.message_handler(commands=['site'])
+async def cmd_site(message: types.Message):
+    await message.answer("Текст", reply_markup=keyboard)
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer("Добро пожаловать!", reply_markup=keyboard)
 
 
