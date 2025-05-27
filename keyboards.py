b72986d7d7547e8f04665b19c76e12d8a87c8568
Hitslater <86098865+Hitslater@users.noreply.github.com>
2025-05-27 20:55:46 +0300

    Add files via upload

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,5 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
-from .keyboards import keyboard
 
 web_app = WebAppInfo(url="https://hitslater.github.io/")
 
@@ -8,16 +7,5 @@ keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
         [types.KeyboardButton(text="Site", web_app=web_app)]
     ],
-    
     resize_keyboard=True
 )
-
-@dp.message_handler(commands=['site'])
-async def cmd_site(message: types.Message):
-    await message.answer("Текст", reply_markup=keyboard)
-
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message):
-    await message.answer("Добро пожаловать!", reply_markup=keyboard)
-
-
